009e76ff7d01a6c8ba45f7104943990952845d20
Hundaol Negusie <hundaolnk@gmail.com>
2022-10-15 09:12:58 +0300

    added loaders to account

--- src/pages/Account/userOrder.jsx
+++ src/pages/Account/userOrder.jsx
@@ -1,20 +1,14 @@
 import React, {useEffect} from "react";
 
-
 import { useDispatch, useSelector } from 'react-redux';
 import { getOrdersbyUser, getOrdersComplete } from '../../redux/actions/orderActions';
- 
-
-import classes from './account.module.scss'
 import { useCookies } from "react-cookie";
-
-
-//antd icon import
-import {ExclamationCircleOutlined } from '@ant-design/icons'
-//antd collapse imports
+ 
+import classes from './account.module.scss'
 
 import { Collapse } from 'antd';
 
+import { CircularProgress } from "@mui/material";
 const { Panel } = Collapse;
 import Row from './orderDetail';
 
@@ -33,6 +27,7 @@ export default function UserOrder(params) {
   },[])
   
   const orders = useSelector((state) => state.order.orders);
+  const orderLoad = useSelector((state) => state.order.loading);
 
   console.log('order info from user order file ')
   // orders.map((val, index)=> console.log(val))
@@ -41,87 +36,47 @@ export default function UserOrder(params) {
 
     <>
       <div className={classes.userOrderHolder}>
-
-          <Collapse accordion>
-              {
-                 !orders?.length ? <div>No Orders</div> : 
-                 (
+        <Collapse accordion>
+          {!orderLoad ? 
+            <>
+              {!orders?.length ? 
+                <div>No Previous Orders</div> 
+              : 
+                (
                   orders?.map((val , index)=>{
-                      return(
-                        <>
-                          <Panel header={`Order ID : ${val._id}`} key={index}>
-                              <Row 
-                                  key = {val._id}   
-                                  id = {val._id}
-                                  fname = {val.fname}
-                                  lname = {val.lname}
-                                  contact = {val.contact} 
-                                  user = {val.userId}
-                                  total = {val.total}
-                                  date = {val.date}
-                                  status = {val.status}
-                                  address = {val.address}
-                                  orders = {val.orders}
-                                  longitude = {val.longitude}
-                                  latitude = {val.latitude}
-                                  admin = {true}
-                                  />
-                          </Panel>
-                        </>
-                      )
+                    return(
+                      <>
+                        <Panel header={`Order ID : ${val._id}`} key={index}>
+                          <Row 
+                            key = {val._id}   
+                            id = {val._id}
+                            fname = {val.fname}
+                            lname = {val.lname}
+                            contact = {val.contact} 
+                            user = {val.userId}
+                            total = {val.total}
+                            date = {val.date}
+                            status = {val.status}
+                            address = {val.address}
+                            orders = {val.orders}
+                            longitude = {val.longitude}
+                            latitude = {val.latitude}
+                            admin = {true}
+                            />
+                        </Panel>
+                      </>
+                    )
                   })
-                 )
-             
+                )
               }
-        
-            </Collapse>
-      </div>
-      
-
-
-      {/* <TableContainer component={Paper}>
-        <Table aria-label="collapsible table">
-          <TableHead >
-            <TableRow>
-              <TableCell />
-              
-              <TableCell>Date</TableCell>
-              <TableCell>Address</TableCell>
-            
-              <TableCell align="right">
-                Sub-Total</TableCell>
-              <TableCell align="right">Status</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-          {
-            !orders?.length ? <div>No Orders</div> : (
-              orders.map((val, key) => {
-                console.log(val);
-                return (
-                  <Row 
-                    key = {val._id}   
-                    id = {val._id}
-                    fname = {val.fname}
-                    lname = {val.lname}
-                    contact = {val.contact} 
-                    user = {val.userId}
-                    total = {val.total}
-                    date = {val.date}
-                    status = {val.status}
-                    address = {val.address}
-                    orders = {val.orders}
-                    longitude = {val.longitude}
-                    latitude = {val.latitude}
-                    admin = {true}
-                    />
-                  )
-              }
-            ))
+            </>
+          :
+            <div className={classes.userOrderHolder__load}>
+              <CircularProgress />
+            </div>
           }
-          </TableBody>
-        </Table>
-      </TableContainer> */}
+        </Collapse>
+      </div>
     </>
   )
 };
